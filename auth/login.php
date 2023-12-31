cfcc7bb7b7f33115e5ce4fb3de4469d9debc3fbb
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-01-01 00:32:54 +0100

    feat(login): Implement user login functionality with robust input validation and secure password handling
    ** Details:**
    
    Core login flow implemented: Handles email and password submission, database query, and password verification.
    Input validation: Ensures both email and password fields are filled to prevent empty submissions.
    Secure password storage: Uses password_verify() for reliable password hashing and comparison.
    Consistent error handling: Provides clear feedback to the user via JavaScript alerts for incorrect login attempts or missing fields.
    Code readability: Uses meaningful variable names and comments for better maintainability.
    ** Next Steps:**
    
    Session management: Implement session handling to maintain user state after successful login.
    Input sanitization: Sanitize user input to prevent potential vulnerabilities like SQL injection.
    CSRF protection: Incorporate Cross-Site Request Forgery (CSRF) protection to enhance security.
    Refactoring for clarity: Consider revisiting code structure for potential improvements in readability and organization.

---
+++ auth/login.php
@@ -0,0 +1,57 @@
+<?php require "../includes/header.php"; ?>
+<?php require "../config/config.php"; ?>
+
+<?php 
+    if(isset($_POST["submit"])){
+
+        if(empty($_POST["email"]) OR empty($_POST["password"])) {
+            echo "<script>alert('Something is empty');</script>";
+        } else{
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+
+            $login = $conn->query("SELECT * FROM users WHERE email='$email'");
+            $login->execute();
+            $fetch = $login->fetch(PDO::FETCH_ASSOC);
+
+            if($login->rowCount() > 0){
+                if(password_verify($password, $fetch["mypassword"])){
+                    echo "login";
+            } else{
+                echo "<script>alert('Incorrect password or email');</script>";
+            }
+        } else{
+            echo "<script>alert('Incorrect password or email');</script>";
+        }
+        }
+
+    }
+?>
+
+
+
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <form class="form-control mt-5" method="POST" action="login.php">
+                    <h4 class="text-center mt-3"> Login </h4>
+                   
+                    <div class="">
+                        <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+                        <div class="">
+                            <input type="email" name="email" class="form-control">
+                        </div>
+                    </div>
+                    <div class="">
+                        <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+                        <div class="">
+                            <input type="password" name="password" class="form-control" id="inputPassword">
+                        </div>
+                    </div>
+                    <button name="submit" class="w-100 btn btn-lg btn-primary mt-4 mb-4" type="submit">login</button>
+
+                </form>
+            </div>
+        </div>
+ 
+   
+        <?php require "../includes/footer.php"; ?>
