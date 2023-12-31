e42fa01d1f43a23b4ab9267c1e04f528ce20da8d
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-12-31 10:29:31 +0100

    commented...

--- config/config.php
+++ config/config.php
@@ -10,9 +10,9 @@ $pass = "";
 $conn = new PDO("mysql:host=$host; dbname=$dbname", $user, $pass);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-if($conn){
-    echo"<h1>Connection worked</h1>";
-} else{
-    echo "<h3>Error in db connection.</h3>";
-}
+// if($conn){
+//     echo"<h1>Connection worked</h1>";
+// } else{
+//     echo "<h3>Error in db connection.</h3>";
+// }
 ?>
