ee8f4c4c42f17d5579e2be3e39545bbf7901e72c
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-12-31 10:06:39 +0100

    configuration file done...

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+$host = 'localhost';
+
+$dbname = "bookstore";
+
+$user = 'root';
+
+$pass = "";
+
+$conn = new PDO("mysql:host=$host; dbname=$dbname", $user, $pass);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if($conn){
+    echo"<h1>Connection worked</h1>";
+} else{
+    echo "<h3>Error in db connection.</h3>";
+}
+?>
