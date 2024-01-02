e888876c51570e5f1f78a6097890d9392359033b
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-01-02 14:21:43 +0100

    commenting code so as to do the most important things....

--- shopping/cart.php
+++ shopping/cart.php
@@ -99,3 +99,70 @@ $allProducts = $products->fetchAll(PDO::FETCH_OBJ);
     </div>
      
     <?php require "../includes/footer.php"; ?>
+
+
+    
+<script>
+    $(document).ready(function(){
+          
+      $(".pro_amount").mouseup(function () {
+                  
+                  var $el = $(this).closest('tr');
+
+        
+
+                  var pro_amount = $el.find(".pro_amount").val();
+                  var pro_price = $el.find(".pro_price").html();
+
+                  var total = pro_amount * pro_price;
+                  $el.find(".total_price").html("");        
+
+                  $el.find(".total_price").append(total+'$');
+
+                  // $(".btn-update").on('click', function(e) {
+
+                  //     var id = $(this).val();
+                    
+
+                  //     $.ajax({
+                  //       type: "POST",
+                  //       url: "update-item.php",
+                  //       data: {
+                  //         update: "update",
+                  //         id: id,
+                  //         product_amount: pro_amount
+                  //       },
+
+                  //       success: function() {
+                  //        // alert("done");
+                  //         reload();
+                  //       }
+                  //     })
+                  //   });
+                 
+                
+          //  fetch();     
+      });
+
+      // fetch();
+
+      // function fetch() {
+
+      //   setInterval(function () {
+      //             var sum = 0.0;
+      //             $('.total_price').each(function()
+      //             {
+      //                 sum += parseFloat($(this).text());
+      //             });
+      //             $(".full_price").html(sum+"$");
+      //   }, 4000);
+      // } 
+      
+      // function reload() {
+
+       
+      //       $("body").load("cart.php")
+       
+      // }
+});
+</script>
