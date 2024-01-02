666a03496dda42f9aaa718822968b4dcb2bf3c44
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-01-02 11:04:26 +0100

    jquery cdn was added ...

--- includes/footer.php
+++ includes/footer.php
@@ -70,6 +70,7 @@
         </div>
         <!-- Copyright -->
         </footer>
+        <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="" crossorigin="anonymous"></script>
   </body>
