62d3e371a21e217b2632a9e8393634f866785237
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-01-02 17:36:04 +0100

    Handles Quantity Updates from the Cart Page: Receives an AJAX POST request when a quantity is modified on the cart page. Extracts the updated product ID and quantity from the request data. Updates the corresponding pro_amount in the cart table using a prepared statement.

---
+++ shopping/update-item.php
@@ -0,0 +1,18 @@
+<?php require "../includes/header.php"; ?>
+<?php require "../config/config.php"; ?>
+
+<?php 
+
+if(isset($_POST["update"])){
+    $id = $_POST['id'];
+    $pro_amount = $_POST['pro_amount'];
+
+    $update = $conn->prepare("UPDATE cart SET pro_amount='$pro_amount' WHERE id='$id'");
+
+    $update->execute();
+
+}
+
+?>
+
+<?php require "../includes/footer.php"; ?>
