23e03f06cc0f94961604e74587234e5745c67d56
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-01-06 13:08:28 +0100

    changed to BookShop

--- includes/header.php
+++ includes/header.php
@@ -24,7 +24,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-dark" >
     <div class="container" style="margin-top: none">
-        <a class="navbar-brand  text-white" href="<?php echo APPURL; ?>">Bookstore</a>
+        <a class="navbar-brand  text-white" href="<?php echo APPURL; ?>">BookShop</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
@@ -77,4 +77,4 @@
     </div>
     </nav>
 
-    <div class="container">  
+    <div class="container">
